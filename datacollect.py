9a002668d00beac8ebbdf568c011d68df0af7de6
jacoab <jjongewaard1@gmail.com>
2020-12-03 13:24:37 -0700

    Added AMD data files and updates exploration notebook

--- datacollect.py
+++ datacollect.py
@@ -11,9 +11,9 @@ start_date = "2010-01-01"
 end_date = "2020-01-01"
 
 print('Starting data collection...')
-amazon_data = quandl.get('WIKI/AMZN', start_date=start_date, end_date=end_date)
+amazon_data = quandl.get('WIKI/AMD', start_date=start_date, end_date=end_date)
 shopify_data = quandl.get('WIKI/NVDA', start_date=start_date, end_date=end_date)
 
 print('Writing data to disk...')
-amazon_data.to_csv('amazon')
+amazon_data.to_csv('amd')
 shopify_data.to_csv('nvidia')
