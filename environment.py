dea9cd1622196161445a8d3006efd43949d25b9a
Jacoab <jjongewaard1@gmail.com>
2020-12-08 01:54:26 -0700

    Fixed environment bug

--- environment.py
+++ environment.py
@@ -15,9 +15,11 @@ ActionType = ['BUY', 'SELL', 'HOLD', '']
 
 class Environment:
 
-    def __init__(self, funds, max_shares, *argv):
+    def __init__(self, funds, max_shares, trial_len, *argv):
+        n = random.randint(0, len(argv[0]) - trial_len)
+
         self.step_num = 0
-        self.day_index = self.step_num + 4
+        self.day_index = n + 4
 
         holdings = [0 for _ in argv]
         opening_prices = [arg['Open'][self.day_index] for arg in argv]
@@ -29,7 +31,7 @@ class Environment:
         self.buyable_stocks = {stock.name: stock for stock in argv}
         self.holdings_name_list = list(self.buyable_stocks.keys())
         self.num_of_equities = len(argv)
-        self.prev_5_opens = np.array([stock['Open'][0:5] for stock in argv])
+        self.prev_5_opens = np.array([stock['Open'][n:n+5] for stock in argv])
 
         self.state = np.array([funds, 0.0] + holdings + opening_prices + opening_avgs)
         self.holdings_range = (2, 2 + len(argv))
@@ -40,16 +42,6 @@ class Environment:
 
         self.set_past_5_days_open_avg(self.calc_past_5_days_open_avg())
 
-    '''
-    def reset(self, funds):
-        holdings = [0 for _ in range(self.num_of_equities)]
-        opening_prices = [stock['Open'][0] for stock in self.buyable_stocks]
-        opening_avgs = [0.0 for _ in range(self.num_of_equities)]
-
-        self.state = np.array([self.init_funds, 0.0] + holdings + opening_prices + opening_avgs)
-        self.step = 0
-    '''
-
     def step(self, action_type, stock, amount_of_shares):
         if action_type not in ActionType:
             raise ValueError('{} is not a valid action type'.format(action_type))
@@ -57,7 +49,7 @@ class Environment:
             raise ValueError('{} is not a valid amount of shares to buy'.format(amount_of_shares))
 
         if action_type == 'HOLD':
-            reward = self.calc_sparse_reward(self.get_funds())
+            reward = self.partial_update_state(self.get_funds())
             return self.state, reward, False
 
         price_per_share = self.buyable_stocks[stock]['Open'][self.day_index]
@@ -78,6 +70,10 @@ class Environment:
         else:
             pass
 
+        reward = self.partial_update_state(funds)
+        return self.state, reward, False
+
+    def partial_update_state(self, funds):
         self.step_num += 1
         self.day_index += 1
 
@@ -87,9 +83,8 @@ class Environment:
         past_5_day_avgs = self.calc_past_5_days_open_avg()
         self.set_past_5_days_open_avg(past_5_day_avgs)
 
-        reward = self.calc_sparse_reward(funds)
-
-        return self.state, reward, False
+        reward = self.calc_complex_reward(funds)
+        return reward
 
     def buy(self, stock, shares, price_per_share):
         funds = self.get_funds()
@@ -126,6 +121,24 @@ class Environment:
 
         if reward > 0:
             reward += 1    # positive rewards are rare so they should be worth more
+
+        return reward
+
+    def calc_complex_reward(self, prev_funds):
+        reward = (self.get_funds() - prev_funds) / self.init_funds
+        averages = self.get_past_5_days_open_avg()
+        opens = self.get_opening_prices()
+        holdings = self.get_holdings()
+
+        if reward > 0:
+            reward += 1.0    # positive rewards are rare so they should be worth more
+
+        holdings_values = opens * holdings
+        average_holdings = averages * holdings
+
+        for i in range(self.num_of_equities):
+            if holdings_values[i] > average_holdings[i]:
+                reward += 0.5
 
         return reward
 
